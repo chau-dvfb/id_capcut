633e47f0b00fab7e5daea9463cfdf7de93c7665a
chau-dvfb <chaudvfb2002@gmail.com>
2024-04-13 06:47:39 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tool lấy ID CapCut</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <style>
+    body {
+      background-color: #f8f9fa; 
+    }
+    .container {
+      margin-top: 100px; 
+    }
+    .card {
+      background-color: #ffffff; 
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+    }
+    .form-group {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <h5 class="card-title text-center">Công cụ miễn phí giúp bạn dễ dàng lấy ID CapCut</h5>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="facebookLink">Vui lòng nhập link CapCut của bạn:</label>
+              <input type="text" class="form-control" id="facebookLink" placeholder="Nhập link Facebook">
+            </div>
+            <button type="button" class="btn btn-primary btn-block" onclick="getFacebookID()">Lấy ID</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    function getFacebookID() {
+      var facebookLink = document.getElementById("facebookLink").value;
+      var decoded_url = decodeURIComponent(facebookLink);
+      var user_id_pattern = /user_id=(\d+)/;
+      var match = decoded_url.match(user_id_pattern);
+      
+      if (match) {
+        var user_id = match[1];
+        document.getElementById("facebookLink").value = user_id; 
+        var successMessage = document.createElement("div");
+        successMessage.classList.add("alert", "alert-success");
+        successMessage.textContent = "Lấy ID thành công!";
+        document.querySelector(".card-body").appendChild(successMessage);
+      } else {
+        alert("Lấy ID thất bại!.");
+      }
+    }
+  </script>
+</body>
+</html>
